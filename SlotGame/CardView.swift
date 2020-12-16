a1afdf1824b83214531e7ecfd95daa7c74f73435
NuriddinJumaev <nuriddin.jumaev98>
2020-12-16 14:44:06 +0500

    Project Done

---
+++ SlotGame/CardView.swift
@@ -0,0 +1,28 @@
+//
+//  CardView.swift
+//  SlotGame
+//
+//  Created by Nuriddin Jumaev on 12/16/20.
+//
+
+import SwiftUI
+
+struct CardView: View {
+    
+    @Binding var symbol:String
+    @Binding var background:Color
+    
+    var body: some View {
+        
+        Image(symbol)
+            .resizable()
+            .aspectRatio(1, contentMode: .fit)
+            .background(background.opacity(0.5))
+            .cornerRadius(20)    }
+}
+
+struct CardView_Previews: PreviewProvider {
+    static var previews: some View {
+        CardView(symbol: Binding.constant("cherry"), background: Binding.constant(Color.green))
+    }
+}
